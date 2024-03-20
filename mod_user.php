251b3ae44381c59d160b98c7a9ce8591f3ac0ce7
DreamT92 <gianluca_p92@hotmail.it>
2024-03-20 22:25:52 +0100

    Update mod_user.php
    Update user modification program

--- mod_user.php
+++ mod_user.php
@@ -3,27 +3,28 @@ include "conn_db.php";
 
 $json_dati = file_get_contents('php://input');
 $dati_user = json_decode($json_dati, true);
+
 $email = $dati_user['email'];
-$first_name = $dati_user['firstName'];
-$last_name = $dati_user['lastName'];
+$firstName = $dati_user['firstName'];
+$lastName = $dati_user['lastName'];
 $username = $dati_user['username'];
 $passw = hash('sha256', $dati_user['passw']);
 $img = $dati_user['img'];
+$userId=$dati_user['userId'];
 
-$query = "UPDATE user SET firstName = ? WHERE email = ?";   
+$query = "UPDATE user SET email = ?, firstName = ?, lastName = ?, username = ?, passw = ?, img = ? WHERE userId = ?";    
 
 $stmt = $mysqli->prepare($query);
-$stmt->bind_param("ssssss",
+$stmt->bind_param("ssssssi",
     $email, 
     $firstName, 
     $lastName,
     $username,
     $passw,
-    $img
+    $img,
+    $userId
 );
 
 $stmt->execute();
 $affected_rows = $stmt->affected_rows;
 $stmt->close();
-
-echo "<p>Righe modificate: $affected_rows</p>";
