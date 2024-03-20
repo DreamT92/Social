f738756392eae684d2792932d2274784cac5ff87
DreamT92 <gianluca_p92@hotmail.it>
2024-03-20 21:57:54 +0100

    Modify delete_user
    Changed the id parameters

--- delete_user.php
+++ delete_user.php
@@ -3,12 +3,12 @@ include "conn_db.php";
 
 $json_dati = file_get_contents('php://input');
 $dati_user = json_decode($json_dati, true);
-$userId=$dati_user[$userId];
+$userId=$dati_user['userId'];
 
 $query = "DELETE FROM user WHERE userId = ?";
 
 $stmt = $mysqli->prepare($query);
-$stmt->bind_param("s", $email, $firstName, $lastName, $username, $passw, $img);
+$stmt->bind_param("i", $userId);
     
 $stmt->execute();
 $affected_rows = $stmt->affected_rows;
