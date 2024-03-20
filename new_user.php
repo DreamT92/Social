a0e0324cfbc5c9cf93e0519c87cb255975f7fed9
DreamT92 <gianluca_p92@hotmail.it>
2024-03-20 22:12:39 +0100

    Modify new_user
    modified recording program

--- new_user.php
+++ new_user.php
@@ -5,10 +5,10 @@ $json_dati = file_get_contents('php://input');
 $dati_user = json_decode($json_dati, true);
 
 $email = $dati_user['email'];
-$first_name = $dati_user['firstName'];
-$last_name = $dati_user['lastName'];
+$firstName = $dati_user['firstName'];
+$lastName = $dati_user['lastName'];
 $username = $dati_user['username'];
-$passw = hash('sha256', $_POST['passw']);
+$passw = hash('sha256', $dati_user['passw']);
 $img = $dati_user['img'];
 
 $query = "INSERT INTO user (email, firstName, lastName, username, passw, img) VALUES (?, ?, ?, ?, ?, ?)";
