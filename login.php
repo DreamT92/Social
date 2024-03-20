4e3ce85688b5c8a4ba45ee1b4b44f739b4fc5ab8
DreamT92 <gianluca_p92@hotmail.it>
2024-03-20 15:51:49 +0100

    Modify login
    Modified structure to receive the call

--- login.php
+++ login.php
@@ -1,35 +1,26 @@
 <?php
     include "conn_db.php";
-    
-    if ( !isset($_SESSION['logged']) ) {
 
-        if( isset($_POST['go_login']) && $_POST['go_login'] == 1 ) {
+    $json_dati = file_get_contents('php://input');
+    $dati_user = json_decode($json_dati, true);
+    $username=$dati_user['username'];
+    $passw=$dati_user['passw'];
             
-            $passw=md5($_POST['passw']);
-            
-            $query = "SELECT * FROM user WHERE username = ? AND passw = ?";
-            $stmt = $mysqli->prepare($query);
-            $stmt->bind_param("ss", $_POST['username'], $passw); 
-            $stmt->execute();
+    $query = "SELECT * FROM user WHERE username = ? AND passw = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("ss", $username, $passw); 
+    $stmt->execute();
 
-            $result_usr = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-            $stmt->close();
+    $result_usr = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    $stmt->close();
 
-            if ( is_array($result_usr) && count($result_usr) == 1 ) {
-                echo "Ciao ". $result_usr[0]['username'];
-
-                $_SESSION['logged'] = 1;
-
-            } 
-             else {
-                echo "Nessun utente trovato";
-            }
-
-        } else {
-        ?>
-            <!--Login-->
-        <?php
-        }
+    if ( is_array($result_usr) && count($result_usr) == 1 ) {
+        echo "Ciao ". $result_usr[0]['username'];
+        $json_user = json_encode($result_usr);
+        return $json_user;
 
     } 
+    else {
+            echo "Nessun utente trovato";
+    }
 ?>
